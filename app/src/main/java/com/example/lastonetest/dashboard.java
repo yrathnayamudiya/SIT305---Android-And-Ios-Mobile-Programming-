0311398f62af8bfe931db0293982f3fdc806f9a2
samadhi <sammabandara@gmail.com>
2020-04-25 11:25:34 +1000

    Added Google Login

---
+++ app/src/main/java/com/example/lastonetest/dashboard.java
@@ -0,0 +1,21 @@
+package com.example.lastonetest;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+public class dashboard extends AppCompatActivity {
+    private ActionBar toolbar;
+    public static String imi_id=null;
+    public static String sim_id=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
