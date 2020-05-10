bca60d341a79239a91f9df71c50622caef5db58a
samadhi <sammabandara@gmail.com>
2020-05-10 13:24:45 +1000

    Connected Firebase and Inserted Data to database

---
+++ app/src/main/java/com/example/lastonetest/class_user.java
@@ -0,0 +1,53 @@
+package com.example.lastonetest;
+
+public class class_user {
+
+    private  String nic;
+    private String cus_name;
+    private String address;
+    private String phonenumber;
+    private String google_id;
+
+    public class_user() {
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getCus_name() {
+        return cus_name;
+    }
+
+    public void setCus_name(String cus_name) {
+        this.cus_name = cus_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getGoogle_id() {
+        return google_id;
+    }
+
+    public void setGoogle_id(String google_id) {
+        this.google_id = google_id;
+    }
+}
--- app/src/main/java/com/example/lastonetest/user.java
+++ app/src/main/java/com/example/lastonetest/user.java
@@ -19,13 +19,56 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class user extends Fragment {
-
+    public  static EditText firstname;
+    public static EditText nic;
+    public static EditText address;
+    public static EditText number;
+    String personId;
+    Button register;
+    class_user UserRegister;
+    DatabaseReference reff;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View contectView = inflater.inflate(R.layout.user, container, false);
+        FirebaseApp.initializeApp(getActivity());
+        firstname = (EditText)contectView.findViewById(R.id.customer_name);
+        nic = (EditText)contectView.findViewById(R.id.customer_id);
+        address =  (EditText)contectView.findViewById(R.id.cusomer_address_input);
+        number =(EditText)contectView.findViewById(R.id.customer_number);
+        register = (Button)contectView.findViewById(R.id.button);
+        reff = FirebaseDatabase.getInstance().getReference().child("customers");
+
+        UserRegister = new class_user();
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+        if (acct != null) {
+            personId = acct.getId();
+
+        }
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fname = firstname.getText().toString();
+                String addre = address.getText().toString();
+                String contactnum = number.getText().toString();
+                String customer_id_number = nic.getText().toString();
+                UserRegister.setCus_name(fname);
+                UserRegister.setNic(customer_id_number);
+                UserRegister.setAddress(addre);
+                UserRegister.setPhonenumber(contactnum);
+                UserRegister.setGoogle_id(personId);
+                reff.child(customer_id_number).setValue(UserRegister);
+
+                firstname.setText("");
+                address.setText("");
+                number.setText("");
+                nic.setText("");
+                Toast.makeText(getActivity(), "Customer Registration Success", Toast.LENGTH_LONG).show();
 
 
+            }
+        });
 
         return contectView;
 
