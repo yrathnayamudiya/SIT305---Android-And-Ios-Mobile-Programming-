b6ecced82f4bbc558f933c7b6a2f3c0c1de2fc06
samadhi <sammabandara@gmail.com>
2020-05-14 09:29:46 +1000

    Added Search User Layout And Fragment Classes

---
+++ app/src/main/java/com/example/lastonetest/view.java
@@ -0,0 +1,35 @@
+package com.example.lastonetest;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class view extends Fragment {
+    View contectView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        contectView= inflater.inflate(R.layout.view,container,false);
+
+
+
+
+        return contectView;
+    }
+}
