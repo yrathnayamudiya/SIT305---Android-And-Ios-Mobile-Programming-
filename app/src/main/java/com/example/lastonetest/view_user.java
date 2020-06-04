9a460a62f601bbb7013dd2a952717dd75fe4a39f
samadhi <sammabandara@gmail.com>
2020-06-04 11:42:35 +1000

    Added Seach Function with firebase to Search Users

--- app/src/main/java/com/example/lastonetest/view_user.java
+++ app/src/main/java/com/example/lastonetest/view_user.java
@@ -21,12 +21,54 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class view_user extends Fragment {
- 
+    String uid = "";
+    Button serch;
+    TextView name;
+    EditText search;
+    TextView nic;
+    TextView address;
+    TextView phone;
     View contectView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         contectView= inflater.inflate(R.layout.view_user,container,false);
+        search = (EditText)contectView.findViewById(R.id.phonenumber);
+        name = (TextView)contectView.findViewById(R.id.name);
+        nic = (TextView)contectView.findViewById(R.id.nic);
+        address =(TextView)contectView.findViewById(R.id.address);
+        phone =(TextView)contectView.findViewById(R.id.telnu);
+        serch = contectView.findViewById(R.id.button2);
+        final ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String va_address = dataSnapshot.child("address").getValue(String.class);
+                String v_name = dataSnapshot.child("cus_name").getValue(String.class);
+                String v_nic = dataSnapshot.child("nic").getValue(String.class);
+                String v_phone = dataSnapshot.child("phonenumber").getValue(String.class);
+                name.setText(v_name);
+                address.setText(va_address);
+                nic.setText(v_nic);
+                phone.setText(v_phone);
+                Toast.makeText(getActivity(),"Data Retrived Success",Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("TAG", databaseError.getMessage()); //Don't ignore errors!
+            }
+        };
+        serch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uid = search.getText().toString();
+                DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference uidRef = rootRef.child("customers").child(uid);
+                uidRef.addListenerForSingleValueEvent(valueEventListener);
+            }
+        });
+
 
 
 
